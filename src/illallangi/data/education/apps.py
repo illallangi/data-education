902ef1678c061ea8038101e3ab58b0ed1ff10f07
Andrew Cole <andrew.cole@illallangi.com>
2024-10-23 23:20:24 +1100

    feat(illallangi-django-data): updated to use django.models for model list

--- src/illallangi/data/education/apps.py
+++ src/illallangi/data/education/apps.py
@@ -1,42 +1,55 @@
 from django.apps import AppConfig
 from django.conf import settings
+from django.db.models.signals import post_migrate
 
-from illallangi.django.data.models.models_manager import ModelsManager
 from illallangi.rdf.adapters import EducationAdapter as RDFAdapter
+
+
+def add_model(
+    **_kwargs: dict[str, object],
+) -> None:
+    from illallangi.django.data.models import Model, Synchronize
+
+    Model.objects.update_or_create(
+        description="Each lesson unlocks new doors to knowledge, empowering you to shape your future.",
+        icon="education/courses.jpg",
+        model="illallangi.data.education.models.Course",
+        plural="Courses",
+        singular="Course",
+        url="courses_html",
+    )
+
+    Synchronize.objects.update_or_create(
+        callable="illallangi.data.education.apps.synchronize",
+    )
 
 
 class EducationalHistoryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "illallangi.data.education"
 
-    def ready(self) -> None:
-        ModelsManager().add_model(
-            url="courses_html",
-            singular="Course",
-            plural="Courses",
-            icon="education/courses.jpg",
-            description="Each lesson unlocks new doors to knowledge, empowering you to shape your future.",
-            model="illallangi.data.education.models.Course",
+    def ready(
+        self,
+    ) -> None:
+        post_migrate.connect(
+            add_model,
+            sender=self,
         )
 
-        def synchronize() -> None:
-            from illallangi.data.education.adapters import (
-                EducationAdapter as DjangoAdapter,
-            )
 
-            src = RDFAdapter(
-                **settings.RDF,
-            )
-            dst = DjangoAdapter()
+def synchronize() -> None:
+    from illallangi.data.education.adapters import (
+        EducationAdapter as DjangoAdapter,
+    )
 
-            src.load(
-                **settings.EDUCATION,
-            )
-            dst.load()
+    src = RDFAdapter(
+        **settings.RDF,
+    )
+    dst = DjangoAdapter()
 
-            src.sync_to(dst)
+    src.load(
+        **settings.EDUCATION,
+    )
+    dst.load()
 
-        ModelsManager().add_synchronize(
-            name=__name__,
-            synchronize=synchronize,
-        )
+    src.sync_to(dst)
